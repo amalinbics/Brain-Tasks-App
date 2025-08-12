d6b89777daf4b44e684e7f4bb4a7a1c66a33b253
amal <amalinbics@gmail.com>
2025-08-12 14:48:54 +0000

    updated image uri dynamically in deployment.yaml

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,10 +3,10 @@ set -e
 echo "Running ApplicationStart hook..."
 
 # Get the ECR image URI from the CodeBuild artifact
-IMAGE_URI=$(jq -r '.[0].imageUri' /tmp/codedeploy-app/imagedefinitions.json)
+#IMAGE_URI=$(jq -r '.[0].imageUri' /tmp/codedeploy-app/imagedefinitions.json)
 
 # Replace the image URI in the deployment YAML file
-sed -i "s|<IMAGE_URI>|${IMAGE_URI}|g" /tmp/codedeploy-app/deployment.yaml
+#sed -i "s|<IMAGE_URI>|${IMAGE_URI}|g" /tmp/codedeploy-app/deployment.yaml
 
 # Apply the Kubernetes manifest files
 kubectl apply -f /tmp/codedeploy-app/deployment.yaml
